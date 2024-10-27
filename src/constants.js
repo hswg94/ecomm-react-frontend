abe7a05bfb30fff7a85aa915e3261234ff2c59fe
hswg94 <hswg94@gmail.com>
2024-10-28 01:59:34 +0800

    changed constants.js to test out

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const BASE_URL = '';
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
-export const UPLOAD_URL = '/api/upload';
+export const PRODUCTS_URL = 'https://api.hswg94.com/api/products';
+export const USERS_URL = 'https://api.hswg94.com/api/users';
+export const ORDERS_URL = 'https://api.hswg94.com/api/orders';
+export const PAYPAL_URL = 'https://api.hswg94.com/api/config/paypal';
+export const UPLOAD_URL = 'https://api.hswg94.com/api/upload';
