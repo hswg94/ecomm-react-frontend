dfbb868baf6fd8a9910565a4f8b451a6cdf00a51
hswg94 <hswg94@gmail.com>
2024-10-28 23:46:59 +0800

    added commented out sections

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,17 @@ export const USERS_URL = 'https://api.hswg94.com/api/users';
 export const ORDERS_URL = 'https://api.hswg94.com/api/orders';
 export const PAYPAL_URL = 'https://api.hswg94.com/api/config/paypal';
 export const UPLOAD_URL = 'https://api.hswg94.com/api/upload';
+
+// export const BASE_URL = 'http://localhost:5000';
+// export const PRODUCTS_URL = 'http://localhost:5000/api/products';
+// export const USERS_URL = 'http://localhost:5000/api/users';
+// export const ORDERS_URL = 'http://localhost:5000/api/orders';
+// export const PAYPAL_URL = 'http://localhost:5000/api/config/paypal';
+// export const UPLOAD_URL = 'http://localhost:5000/api/upload';
+
+// export const BASE_URL = '';
+// export const PRODUCTS_URL = '/api/products';
+// export const USERS_URL = '/api/users';
+// export const ORDERS_URL = '/api/orders';
+// export const PAYPAL_URL = '/api/config/paypal';
+// export const UPLOAD_URL = '/api/upload';
