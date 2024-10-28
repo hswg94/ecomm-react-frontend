0d90bfaa0393e4cbe5be5098e01caa8138ea7acf
hswg94 <hswg94@gmail.com>
2024-10-28 23:37:30 +0800

    fixed apiSlice so that it includes credentials

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from  '../constants';
-const baseQuery = fetchBaseQuery({baseUrl: BASE_URL});
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: BASE_URL,
+    credentials: 'include', // Ensure cookies are included in all API requests
+  });
+
 
 export const apiSlice = createApi({
     baseQuery,
