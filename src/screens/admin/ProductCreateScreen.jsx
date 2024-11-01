60b8ef03ed50927692c35183e8e1fa9762f0ef44
hswg94 <hswg94@gmail.com>
2024-11-01 22:17:23 +0800

    added an actual page to create a product

---
+++ src/screens/admin/ProductCreateScreen.jsx
@@ -0,0 +1,125 @@
+// src/screens/ProductCreateScreen.js
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import Loader from '../../components/Loader';
+import FormContainer from '../../components/FormContainer';
+import { useCreateProductMutation } from '../../slices/productsApiSlice';
+
+const ProductCreateScreen = () => {
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState('');
+  const [brand, setBrand] = useState('');
+  const [category, setCategory] = useState('');
+  const [countInStock, setCountInStock] = useState(0);
+  const [description, setDescription] = useState('');
+
+  const navigate = useNavigate();
+
+  const [createProduct, { isLoading }] = useCreateProductMutation();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await createProduct({
+        name,
+        price,
+        image,
+        brand,
+        category,
+        countInStock,
+        description,
+      }).unwrap();
+      toast.success('Product created successfully');
+      navigate('/admin/productlist'); // Redirect to the product list page
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  return (
+    <FormContainer>
+      <h1>Create Product</h1>
+      {isLoading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter product name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='price'>
+          <Form.Label>Price</Form.Label>
+          <Form.Control
+            type='number'
+            placeholder='Enter product price'
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='image'>
+          <Form.Label>Image URL</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter image URL'
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='brand'>
+          <Form.Label>Brand</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter brand'
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='category'>
+          <Form.Label>Category</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter category'
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='countInStock'>
+          <Form.Label>Count In Stock</Form.Label>
+          <Form.Control
+            type='number'
+            placeholder='Enter stock count'
+            value={countInStock}
+            onChange={(e) => setCountInStock(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId='description'>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter product description'
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type='submit' variant='primary' className='mt-3'>
+          Create Product
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default ProductCreateScreen;
