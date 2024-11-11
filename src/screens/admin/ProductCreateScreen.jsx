89b9a59c01a0b3838040369c6d657663d328b925
hswg94 <hswg94@gmail.com>
2024-11-11 22:29:08 +0800

    added image upload button to product create screen

--- src/screens/admin/ProductCreateScreen.jsx
+++ src/screens/admin/ProductCreateScreen.jsx
@@ -1,24 +1,29 @@
 // src/screens/ProductCreateScreen.js
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Form, Button } from 'react-bootstrap';
-import { toast } from 'react-toastify';
-import Loader from '../../components/Loader';
-import FormContainer from '../../components/FormContainer';
-import { useCreateProductMutation } from '../../slices/productsApiSlice';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { toast } from "react-toastify";
+import Loader from "../../components/Loader";
+import FormContainer from "../../components/FormContainer";
+import {
+  useCreateProductMutation,
+  useUploadProductImageMutation,
+} from "../../slices/productsApiSlice";
 
 const ProductCreateScreen = () => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
-  const [image, setImage] = useState('');
-  const [brand, setBrand] = useState('');
-  const [category, setCategory] = useState('');
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
   const [countInStock, setCountInStock] = useState(0);
-  const [description, setDescription] = useState('');
-
-  const navigate = useNavigate();
+  const [description, setDescription] = useState("");
 
   const [createProduct, { isLoading }] = useCreateProductMutation();
+  const [uploadProductImage, { isLoading: loadingUpload }] =
+    useUploadProductImageMutation();
+
+  const navigate = useNavigate();
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -32,8 +37,20 @@ const ProductCreateScreen = () => {
         countInStock,
         description,
       }).unwrap();
-      toast.success('Product created successfully');
-      navigate('/admin/productlist'); // Redirect to the product list page
+      toast.success("Product Created!");
+      navigate("/admin/productlist"); // Redirect to the product list page
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData();
+    formData.append("image", e.target.files[0]);
+    try {
+      const res = await uploadProductImage(formData).unwrap();
+      toast.success(res.message);
+      setImage(res.image);
     } catch (err) {
       toast.error(err?.data?.message || err.error);
     }
@@ -44,77 +61,83 @@ const ProductCreateScreen = () => {
       <h1>Create Product</h1>
       {isLoading && <Loader />}
       <Form onSubmit={submitHandler}>
-        <Form.Group controlId='name'>
+        <Form.Group controlId="name">
           <Form.Label>Name</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter product name'
+            type="text"
+            placeholder="Enter product name"
             value={name}
             onChange={(e) => setName(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId='price'>
+        <Form.Group controlId="price">
           <Form.Label>Price</Form.Label>
           <Form.Control
-            type='number'
-            placeholder='Enter product price'
+            type="number"
+            placeholder="Enter product price"
             value={price}
             onChange={(e) => setPrice(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId='image'>
+        <Form.Group controlId="image">
           <Form.Label>Image URL</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter image URL'
+            type="text"
+            placeholder="Enter image URL"
             value={image}
             onChange={(e) => setImage(e.target.value)}
           ></Form.Control>
+          <Form.Control
+            label="Choose File"
+            onChange={uploadFileHandler}
+            type="file"
+          ></Form.Control>
+          {loadingUpload && <Loader />}
         </Form.Group>
 
-        <Form.Group controlId='brand'>
+        <Form.Group controlId="brand">
           <Form.Label>Brand</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter brand'
+            type="text"
+            placeholder="Enter brand"
             value={brand}
             onChange={(e) => setBrand(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId='category'>
+        <Form.Group controlId="category">
           <Form.Label>Category</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter category'
+            type="text"
+            placeholder="Enter category"
             value={category}
             onChange={(e) => setCategory(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId='countInStock'>
+        <Form.Group controlId="countInStock">
           <Form.Label>Count In Stock</Form.Label>
           <Form.Control
-            type='number'
-            placeholder='Enter stock count'
+            type="number"
+            placeholder="Enter stock count"
             value={countInStock}
             onChange={(e) => setCountInStock(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group controlId='description'>
+        <Form.Group controlId="description">
           <Form.Label>Description</Form.Label>
           <Form.Control
-            type='text'
-            placeholder='Enter product description'
+            type="text"
+            placeholder="Enter product description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary' className='mt-3'>
+        <Button type="submit" variant="primary" className="mt-3">
           Create Product
         </Button>
       </Form>
