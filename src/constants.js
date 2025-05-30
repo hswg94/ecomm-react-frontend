0c17b90b185677ec82a62ad9073113e10f4b9d6f
hswg94 <hswg94@gmail.com>
2025-05-30 10:44:37 +0800

    cleaned up local API URLs

--- src/constants.js
+++ src/constants.js
@@ -6,15 +6,8 @@ export const PAYPAL_URL = 'https://api.hswg94.com/api/config/paypal';
 export const UPLOAD_URL = 'https://api.hswg94.com/api/upload';
 
 // export const BASE_URL = 'http://localhost:5000';
-// export const PRODUCTS_URL = 'http://localhost:5000/api/products';
-// export const USERS_URL = 'http://localhost:5000/api/users';
+// export const PRODUCTS_URL = 'http://localhost:5002/api/products';
+// export const USERS_URL = 'http://localhost:5001/api/users';
 // export const ORDERS_URL = 'http://localhost:5000/api/orders';
 // export const PAYPAL_URL = 'http://localhost:5000/api/config/paypal';
 // export const UPLOAD_URL = 'http://localhost:5000/api/upload';
-
-// export const BASE_URL = '';
-// export const PRODUCTS_URL = '/api/products';
-// export const USERS_URL = '/api/users';
-// export const ORDERS_URL = '/api/orders';
-// export const PAYPAL_URL = '/api/config/paypal';
-// export const UPLOAD_URL = '/api/upload';
